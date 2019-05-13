0d3f8e2442a763ba090017c5b8faf470feb6408f
Lay Flags <code@layfla.gs>
2019-05-13 10:57:21 +0200

    make it a PWA
    - setup workbox and generate sw before publishing
    - update deps and fix styles
    - extend manifests with short_name and start_url
    - fix http headers in firebase.json

---
+++ workbox-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "globDirectory": "build/",
+  "globPatterns": [
+    "**/*.{css,html,jpg,js,png,xml,mp4,ico,json,svg}"
+  ],
+  "globIgnores": [
+    "**/_variables.css",
+    "**/cloudy.mp4",
+    "**/safari-pinned-tab.svg"
+  ],
+  "swDest": "build/sw.js"
+};
